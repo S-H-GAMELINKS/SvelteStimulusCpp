4d80b74adea586baae2feb148fe85aa05f9846e3
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-06-05 16:19:31 +0900

    using svelte-routing for spa

--- static/src/App.svelte
+++ static/src/App.svelte
@@ -1,13 +1,22 @@
+<!-- App.svelte -->
 <script>
-	export let name;
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import About from "./routes/About.svelte";
+  import Contact from "./routes/Contact.svelte";
+
+  export let url = "";
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<div data-controller="hoge">
-    <h1>Hello {name}!</h1>
-</div>
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>
+  <div>
+    <Route path="/contact" component="{Contact}" />
+    <Route path="/about" component="{About}" />
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
---
+++ static/src/routes/About.svelte
@@ -0,0 +1,3 @@
+    
+<h1>About</h1>
+<p>I like to code</p>
